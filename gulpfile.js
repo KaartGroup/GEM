42f90ed75781d4d6ebc3de9b63a455ede353dee1
farieberrie <fbilly19@gmail.com>
2020-06-04 14:44:24 -0600

    Update to Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,26 @@ var minifyCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var changed = require('gulp-changed');
+var watch = require('gulp-watch');
 
 var SCSS_SRC = './src/Assets/scss/**/*.scss';
 var SCSS_DEST = './src/Assets/css';
 
-gulp.task('compile_scss',function(){
+function compile_scss(){
 gulp.src(SCSS_SRC)
 .pipe(sass().on('error',sass.logError))
 .pipe(minifyCSS())
 .pipe(rename({suffix: '.min'}))
 .pipe(changed(SCSS_DEST))
 .pipe(gulp.dest(SCSS_DEST));
+}
 
-});
+function watch_css(SCSS_SRC){
+  watch(SCSS_SRC, compile_scss());
+}
 
-gulp.task('watch_scss',function(){
-  gulp.watch(SCSS-SRC, ['compile_scss']);
-});
+function defaultTask(){
+  watch_css();
+}
 
-gulp.task('default',['watch_css']);
+exports.default=defaultTask;
