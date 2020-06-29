5df26acc7cf28550c6263f29b1ea26fc8cc67b8d
farieberrie <fbilly19@gmail.com>
2020-06-29 16:42:28 -0600

    Updated post method and output JSON

--- flask/main.py
+++ flask/main.py
@@ -1,8 +1,10 @@
 from flask import (Flask, render_template, redirect, request)
+from eclass import Editor
 import webview
 import threading
+import json
 
-
+editor = Editor()
 
 app = Flask("__main__")
 
@@ -10,12 +12,29 @@ app = Flask("__main__")
 def my_index():
     return render_template('index.html')
 
-@app.route("/", methods=['POST'] )
+@app.route("/gdat", methods=['GET','POST'] )
 def getdata():
-    if request.method == 'POST':
-        team = request.form('team')
-        print(team)
-    return team
+        if request.method == 'POST':
 
+            req = request.form
+            print(req)
+            
+            editor.team = req["team"]
+            editor.name = req["ename"]
+            editor.username = req["username"]
+            editor.uid = req["editoruid"]
+            editor.linecolor = req["elinecolor"]
+            editor.nodecolor = req["enodecolor"]
+            editor.linewidth = req["elinewidth"]
+            editor.nodesize = req["enodesize"]
+            editor.nodeshape = req["enodeshape"]
+
+            ejson = json.dumps(editor.__dict__)
+            data = open("data.json", "w")
+            data.write(ejson)
+
+            return redirect(request.url)
+
+        return render_template("index.html")
 
 app.run(debug=True)
--- react/src/banner.js
+++ react/src/banner.js
@@ -31,7 +31,6 @@ function startup(){
     enodecol.select(encolor);    
 }
 
-
 function updateFirst(event) {
     var p = document.querySelector("p");
     var b = document.querySelector("button");
@@ -42,7 +41,7 @@ function updateFirst(event) {
 
     if (p) {
         
-      p.style.color = event.target.value;
+        p.style.color = event.target.value;
     }
   }
 
@@ -52,15 +51,13 @@ function updateAll(event) {
 
     });
     document.querySelectorAll("p").forEach(function(p) {
-      p.style.color = event.target.value;
+        p.style.color = event.target.value;
     });
   }
 
-
-
 export function Banner() {
     return (
-       <form action="/." > 
+    <form action="/gdat" method="POST" > 
     <div className="body-style">
     <div className="left-side">
         <p>Team Settings:</p>
@@ -69,72 +66,67 @@ export function Banner() {
         <p >Choose Highlight Colors:</p> 
         <div className="way-values">
         <label for="linecolor"> Line Color: </label> 
-        <input type="color" id="linecolor" name="linecolor" ></input>
+        <input type="color" id="linecolor" name="tlinecolor" ></input>
         <label for="linewidth">Line Width:</label>
-         <input type="number" id="lindewidth" name="linewidth" min="1" max="20"></input>
+         <input type="number" id="lindewidth" name="tlinewidth" min="1" max="20" placeholder="1"></input>
         <label for="nodecolor"> Node Color: </label> 
-            <input type="color" id="nodecolor" name="nodecolor"  ></input>
+            <input type="color" id="nodecolor" name="tnodecolor"  ></input>
         <label for="nodesize">Node Size:</label>
-        <input type="number" id="nodesize" name="nodesize" min="10" max="50" ></input> </div>
-        <label for="nodeshape" >Node Shape:</label>
-            <div className="ebtns-container">
-            <button class="ebtns" type="button" >Circle</button> 
-            <button class="ebtns" type="button" >Triangle</button> 
-            <button class="ebtns" type="button" >Square</button>
-            <button class="ebtns" type="button" >Pentagon</button>
-            <button class="ebtns" type="button" >Hexagon</button>
-            <button class="ebtns"  type="button" >Heptagon</button>
-            <button class="ebtns"  type="button" >Octagon</button>
-            <button class="ebtns"  type="button" >Nonagon</button>
-                        <button class="ebtns"  type="button" >Decagon</button>  
-            </div>  </div>
+        <input type="number" id="nodesize" name="tnodesize" min="10" max="50" placeholder="10"></input> </div>
+        <label for="tnodeshape" >Node Shape:</label>
+        <select name = "tnodeshape">
+            <option value="">--Please Select an Option--</option>
+            <option value="circle">Circle</option>
+            <option value="triangle">Traingle</option>
+            <option value="square">Square</option>
+            <option value="pentagon">Pentagon</option>
+            <option value="hexagon">Hexagon</option>
+            <option value="heptagon">Heptagon</option>
+            <option value="octagon">Octagon</option>
+            <option value="nonagon">Nonagon</option>
+            <option value="decagon">Decagon</option>
+            </select>
+            </div>
     <div className="editor-settings">
         <p>Editor Settings</p>
-            <label for="editorname">Editor Name:</label>
-            <input type="text" id="editorname" placeholder="Enter an Editor's Name" editorname="editorname" required minLength="4" maxLength="16" size="20"></input>
-            <label for="editoruid">Editor User ID:</label>
-            <input type="text" id="editoruid" editorname="editoruid" placeholder="Editor's User ID" required minLength="4" maxLength="16" size="20"></input>
-            <div className="ace-container">
-            <button class="eshape-buttons" type="button" > Add </button>
-            <button class="eshape-buttons" type="button" > Clear </button>
-            <button class="eshape-buttons" type="button" > Edit </button>
-            </div>
+            <label for="ename">Editor Name:</label>
+            <input type="text" id="ename" placeholder="Enter an Editor's Name" name="ename" required minLength="4" maxLength="16" size="20"></input>
+            <label for="username">User Name:</label>
+            <input type="text" id="username" placeholder="Enter a Username" name="username" required minLength="4" maxLength="16" size="20"></input>
+            <label for="editoruid">User ID:</label>
+            <input type="text" id="editoruid" name="editoruid" placeholder="Enter a User ID" required minLength="4" maxLength="16" size="20"></input>
     <div className="editor-way">
         <label for="linecolor"> Line Color: </label> 
-        <input type="color" id="elinecol" name="linecolor" ></input>
+        <input type="color" id="elinecolor" name="elinecolor" ></input>
         <label for="linewidth">Line Width:</label>
-        <input type="number" id="elinewidth" name="linewidth" min="1" max="20"></input>
+        <input type="number" id="elinewidth" name="elinewidth" min="1" max="20" placeholder="1"></input>
         <label for="nodecolor"> Node Color: </label> 
-        <input type="color" id="enodecol" name="nodecolor" ></input>
+        <input type="color" id="enodecolor" name="enodecolor" ></input>
         <label for="nodesize">Node Size:</label>
-        <input type="number" id="enodesize" name="nodesize" min="10" max="50"></input>
+        <input type="number" id="enodesize" name="enodesize" min="10" max="50" placeholder="10"></input>
         </div>
+        <label for="enodeshape" >Node Shape:</label>
+        <select name = "enodeshape">
+            <option value="">--Please Select an Option--</option>
+            <option value="circle">Circle</option>
+            <option value="triangle">Traingle</option>
+            <option value="square">Square</option>
+            <option value="pentagon">Pentagon</option>
+            <option value="hexagon">Hexagon</option>
+            <option value="heptagon">Heptagon</option>
+            <option value="octagon">Octagon</option>
+            <option value="nonagon">Nonagon</option>
+            <option value="decagon">Decagon</option>
+        </select>
         </div>
-    <div className="uidcheckbox"> 
-        <label for="toggleUID">Toggle User ID in the Menu </label>
-        <input type="checkbox" id="toggleUID" name="toggle" ></input>
-        <label for="nodeshape" >Node Shape:</label>
-    <div className="eshape-container">
-        <button class="eshape-buttons" type="button" >Circle</button> 
-        <button class="eshape-buttons" type="button" >Triangle</button> 
-        <button class="eshape-buttons" type="button" >Square</button>
-        <button class="eshape-buttons" type="button" >Pentagon</button>
-        <button class="eshape-buttons" type="button" >Hexagon </button>
-        <button class="eshape-buttons" type="button" >Heptagon</button>
-        <button class="eshape-buttons" type="button" >Octagon</button>
-        <button class="eshape-buttons" type="button" >Nonagon</button>
-        <button class="eshape-buttons" type="button" >Decagon</button>
-        </div>
-        </div>
-    <div className="m-container">
     <p>Table Settings</p>
+    <div className="ebtns-container">
         <button class="export-buttons" type="button" >Remove</button>
         <button class="export-buttons" type="button" >Remove All</button>
         <button class="export-buttons" type="button" >Import</button>
         <button class="export-buttons" type="button" >Export</button>
         <button class="export-buttons" type="submit">Submit</button>
-        </div>
-    </div>
+        </div></div>
     </form>                     
     );
 }
