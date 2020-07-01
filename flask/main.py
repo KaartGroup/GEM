9a9f83f7be5c350007bd0f4227838ebc170650d2
farieberrie <fbilly19@gmail.com>
2020-07-01 16:32:13 -0600

    update import upload and editor class

--- flask/main.py
+++ flask/main.py
@@ -1,40 +1,60 @@
-from flask import (Flask, render_template, redirect, request)
+from flask import (Flask, render_template, redirect, request, jsonify)
 from eclass import Editor
+from werkzeug.utils import secure_filename
 import webview
 import threading
 import json
+import os
+import copy
 
 editor = Editor()
 
+Editorlist = {}
+
 app = Flask("__main__")
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def my_index():
     return render_template('index.html')
 
-@app.route("/gdat", methods=['GET','POST'] )
+@app.route("/add", methods=['GET','POST'] )
 def getdata():
         if request.method == 'POST':
 
             req = request.form
             print(req)
-            
-            editor.team = req["team"]
-            editor.name = req["ename"]
-            editor.username = req["username"]
-            editor.uid = req["editoruid"]
-            editor.linecolor = req["elinecolor"]
-            editor.nodecolor = req["enodecolor"]
-            editor.linewidth = req["elinewidth"]
-            editor.nodesize = req["enodesize"]
-            editor.nodeshape = req["enodeshape"]
 
-            ejson = json.dumps(editor.__dict__)
-            data = open("data.json", "w")
-            data.write(ejson)
+            for key in req:
+                Editorlist[key] = req[key]
+                str(Editorlist)
 
             return redirect(request.url)
 
-        return render_template("index.html")
+        return render_template("index.html", flask_data=(Editorlist))
 
-app.run(debug=True)
+
+
+@app.route("/upload", methods=['GET','POST'])
+def upload():
+    target = os.path.join(APP_ROOT, "uploads/")
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    for file in request.files.getlist("upfile"):
+        print(file)
+        filename = file.filename
+        destination = "/".join((target, filename))
+        print(destination)
+        file.save(destination)
+
+
+    return render_template("index.html")
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
