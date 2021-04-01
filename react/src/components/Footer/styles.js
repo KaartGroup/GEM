b8ba4ac59bcbefe6d5aa426eef5cbe1525e08416
Farieberrie <fbilly19@gmail.com>
2021-04-01 16:28:37 -0600

    update footer

--- react/src/components/Footer/styles.js
+++ react/src/components/Footer/styles.js
@@ -14,7 +14,7 @@ export const MainFooter = styled.div`
   border: 0 solid #333;
   background-size: cover;
   position: relative;
-  margin-top: 70%;
+  margin-top: 100%;
   background-color: #fff;
   padding: 54px 0;
   border-top-width: 3px;
